102d55619bf95472d398e52098e56de05f2826b2
Caterina <137883905+Caterina87@users.noreply.github.com>
2023-09-04 17:00:35 +0200

    add change

--- src/Components/ImageComponent.jsx
+++ src/Components/ImageComponent.jsx
@@ -1,3 +1,6 @@
-const ImageComponent = (props) => <img src={props.src} alt={props.alt} style={{ maxWidth: "100%" }} />;
-
+const ImageComponent = (props) => (
+  <div>
+    <img src={props.src} alt={props.alt} style={{ maxWidth: "100%" }} />;
+  </div>
+);
 export default ImageComponent;
