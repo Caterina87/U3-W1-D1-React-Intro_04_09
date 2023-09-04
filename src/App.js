746c94bc1769823856c4011ffc7ccf80f0035a66
Caterina <137883905+Caterina87@users.noreply.github.com>
2023-09-04 16:47:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ButtonComponent from "./Components/ButtonComponent";
+import ImageComponent from "./Components/ImageComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonComponent btnText="Bottone 1" />
+      <ButtonComponent btnText="Bottone 2" />
+      <ButtonComponent btnText="Bottone 3" />
+      <ImageComponent
+        src="https://images.pexels.com/photos/17984660/pexels-photo-17984660/free-photo-of-bianco-e-nero-donna-danzando-vestito.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        alt="Ballerina"
+      />
     </div>
   );
 }
---
+++ src/Components/ImageComponent.jsx
@@ -0,0 +1,3 @@
+const ImageComponent = (props) => <img src={props.src} alt={props.alt} style={{ maxWidth: "100%" }} />;
+
+export default ImageComponent;
